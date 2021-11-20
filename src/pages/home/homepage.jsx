63d1fff3d895e0b2ba6357adcd6c359e8e573dfe
Zanderson <zack.anderson2011@gmail.com>
2021-11-19 22:11:50 -0600

    Navbar successfully displayed on page..will begin to style

---
+++ src/pages/home/homepage.jsx
@@ -0,0 +1,9 @@
+import SearchAppBar from "../../components/navbar/navbar"
+
+export default function Homepage() {
+    return (
+      <div className="homepage">
+        <SearchAppBar />
+      </div>
+    );
+  }
