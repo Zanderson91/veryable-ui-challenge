3135af62a1120e8d1bd9717b82aa50805629e3c6
Zanderson <zack.anderson2011@gmail.com>
2021-11-19 23:01:28 -0600

    styling card for users

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -14,6 +14,10 @@ import FavoriteIcon from '@mui/icons-material/Favorite';
 import ShareIcon from '@mui/icons-material/Share';
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 import MoreVertIcon from '@mui/icons-material/MoreVert';
+import PersonIcon from '@mui/icons-material/Person';
+import './card.css'
+
+
 
 const ExpandMore = styled((props) => {
   const { expand, ...other } = props;
@@ -34,12 +38,11 @@ export default function UserCard() {
   };
 
   return (
-    <Card sx={{ maxWidth: 345 }}>
+    <Card sx={{ maxWidth: 500 }}>
       <CardHeader
         avatar={
-          <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-            R
-          </Avatar>
+          <PersonIcon fontSize="large" color="#7e7e7e">
+          </PersonIcon>
         }
         action={
           <IconButton aria-label="settings">
@@ -49,12 +52,6 @@ export default function UserCard() {
         title="Shrimp and Chorizo Paella"
         subheader="September 14, 2016"
       />
-      <CardMedia
-        component="img"
-        height="194"
-        image="/static/images/cards/paella.jpg"
-        alt="Paella dish"
-      />
       <CardContent>
         <Typography variant="body2" color="text.secondary">
           This impressive paella is a perfect party dish and a fun meal to cook
@@ -63,12 +60,6 @@ export default function UserCard() {
         </Typography>
       </CardContent>
       <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
         <ExpandMore
           expand={expanded}
           onClick={handleExpandClick}
